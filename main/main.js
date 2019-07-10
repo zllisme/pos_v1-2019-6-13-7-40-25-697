55ca77fd4dab156904995b03da36c3972cecb934
zllisme <1973842936@qq.com>
2019-07-10 23:50:28 +0800

    feat: print receipt

--- main/main.js
+++ main/main.js
@@ -1,3 +1,111 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+const isBarcodeValid = (barcodes) => {
+    let itemBarcodes = [];
+    let allItems = loadAllItems();
+    for(let i = 0; i < allItems.length; i++) {
+        itemBarcodes.push(allItems[i].barcode);
+    }
+    let valid = [];
+    let validBarcodeCount = 0;
+    console.log(itemBarcodes);
+    for(let i = 0; i < barcodes.length; i++) {
+        if(itemBarcodes.indexOf(barcodes[i].split('-')[0]) > -1) {
+            validBarcodeCount++;
+        }
+    }
+    console.log(validBarcodeCount);
+    if(validBarcodeCount == barcodes.length){
+        return true;
+    }
+    return false;
+};
+
+const getBarcodeCount = (barcodes) => {
+    let barcodeCount = {};
+    for(let i = 0; i < barcodes.length; i++) {
+        let eachBarcode = barcodes[i]; 
+        let eachBarcodePrefix = eachBarcode.split("-")[0];
+        let eachBarcodeSuffix = eachBarcode.split("-")[1];
+        if(eachBarcodeSuffix) {
+            barcodeCount[eachBarcodePrefix] = (barcodeCount[eachBarcodePrefix] +
+                parseFloat(eachBarcodeSuffix) ) || parseFloat(eachBarcodeSuffix);
+        }else {
+            barcodeCount[eachBarcodePrefix] = (barcodeCount[eachBarcodePrefix] +1 ) || 1; 
+        }
+    }
+    return barcodeCount;
+};
+
+const getBarcodeDistinct = (barcodeCount) => {
+    let barcodeDistinct = [];
+    for(let barcode in barcodeCount) {
+        barcodeDistinct.push(barcode);
+    }
+    return barcodeDistinct;
+};
+
+const getItemCart = (barcodeDistinct) => {
+    let itemCart = [];
+    let allItems = loadAllItems();
+    for(let i = 0; i < barcodeDistinct.length; i++){
+        for(let j = 0; j < allItems.length; j++) {
+            if(barcodeDistinct[i] === allItems[j]['barcode']) {
+                //delete allItems[i]['barcode'];
+                itemCart.push(allItems[j]);
+                break;
+            }
+        }
+
+    }
+    return itemCart;
+};
+
+const getItemCartPromotion = (barcodeCount, itemCart) => {
+    let itemCartEachPromotion = {};
+    let itemCartPromotion = {};
+    let totalPromotion = 0;
+    let promotion = loadPromotions();
+    for(let i = 0; i < itemCart.length; i++) {
+        let barcode = itemCart[i]['barcode'];
+        let price = itemCart[i]['price'];
+        itemCartEachPromotion[barcode] = 0;
+        for(let j = 0; j < promotion[0]['barcodes'].length; j++){
+            if(barcode == promotion[0]['barcodes'][j] && barcodeCount[barcode] >= 2){
+                itemCartEachPromotion[barcode] = price;
+                totalPromotion += price;
+                break; 
+            }
+        }
+    }
+    itemCartPromotion['total'] = totalPromotion;
+    itemCartPromotion['each'] = itemCartEachPromotion;
+    return itemCartPromotion;
+};
+
+const createReceipt = (barcodeCount, itemCart, itemCartPromotion) => {
+    let receipt = "***<没钱赚商店>收据***\n";
+    let totalAmount = 0;
+    for(let i = 0; i < itemCart.length; i++) {
+        let count = barcodeCount[itemCart[i]['barcode']];
+        let price = itemCart[i]['price']
+        let spend = (count * parseFloat(price) - itemCartPromotion['each'][itemCart[i]['barcode']]).toFixed(2);
+        receipt += `名称：${itemCart[i]['name']}，数量：${count}${itemCart[i]['unit']}，单价：${price.toFixed(2)}(元)，小计：${spend}(元)\n`;
+        totalAmount += parseFloat(spend);
+
+    }
+    receipt += `----------------------\n总计：${totalAmount.toFixed(2)}(元)\n节省：${itemCartPromotion['total'].toFixed(2)}(元)\n**********************`;
+    return receipt;
+};
+
+const printReceipt = (barcodes) => { 
+    if(isBarcodeValid(barcodes)) {
+        console.log("[ERROR]: the barcode list has invalid barcode, please enter again!");
+        return;
+    }   
+    let barcodeCount = getBarcodeCount(barcodes);
+    let barcodeDistinct = getBarcodeDistinct(barcodeCount);
+    let itemCart = getItemCart(barcodesDistinct);
+    let itemCartPromotion = getItemCartPromotion(barcodeCount, itemCart);
+    console.log(createReceipt(barcodeCount, itemCart, itemCartPromotion));
+}
